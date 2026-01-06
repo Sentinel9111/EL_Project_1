35fec8188e31e8bd6c81b106a7d4d3e34f75d43b
Sentinel9111 <92266462+Sentinel9111@users.noreply.github.com>
2026-01-06 17:41:41 +0100

    Trigger mines

--- include/headers.h
+++ include/headers.h
@@ -1,4 +1,7 @@
 #ifndef EL_PROJECT_1_HEADERS_H
 #define EL_PROJECT_1_HEADERS_H
+
 void setupMines();
+void checkMines();
+
 #endif //EL_PROJECT_1_HEADERS_H
--- src/main.cpp
+++ src/main.cpp
@@ -10,16 +10,27 @@
 
 // [12] [13] [14] [15]
 
-// Random mines
-//
+// Randomise mines (DONE)
+// Detect player with LDR
+// Trigger mine when player stands on it
+// Mine deactivates after trigger
+// Timer and dice
+// Multiplexer
 
 #define BOARD_SIZE 16
 #define MINE_COUNT 6
 
 bool hasMine[BOARD_SIZE];
 
+#define LDR_PIN 34 // test ldr
+#define LDR_THRESHOLD 100 // adjust based on light
+
+bool pawnPresent() {
+    int value = analogRead(LDR_PIN);
+    return value < LDR_THRESHOLD; // returns true if pawn is present
+}
+
 void setup() {
-// write your initialization code here
     Serial.begin(9600);
     delay(5000); // Time to open serial monitor
 
@@ -28,16 +39,16 @@ void setup() {
 }
 
 void loop() {
-// write your code here
-
+    checkMines();
 }
 
+// set up a new game
 void setupMines() {
     // Clear board
     for (int i = 0; i < BOARD_SIZE; i++) {
         hasMine[i] = false;
     }
-
+    // Place mines
     int placedMines = 0;
     while (placedMines < MINE_COUNT) {
         int pos = random(0, BOARD_SIZE);
@@ -47,7 +58,7 @@ void setupMines() {
         }
     }
 
-    // Debug output
+    // Print mines
     Serial.print("Mines at positions: ");
     for (int i = 0; i < BOARD_SIZE; i++) {
         if (hasMine[i]) {
@@ -56,7 +67,7 @@ void setupMines() {
         }
     }
     Serial.println();
-
+    // Print mines in grid
     Serial.println("Mine grid:");
     for (int row = 0; row < 4; row++) {
         for (int col = 0; col < 4; col++) {
@@ -65,4 +76,19 @@ void setupMines() {
         }
         Serial.println();
     }
+}
+
+
+bool previousState = false;
+bool mineTriggered = false;
+void checkMines() {
+    if (mineTriggered) return; // only trigger a mine once
+
+    bool currentState = pawnPresent(); // only trigger a mine when a pawn arrives
+
+    if (!previousState && currentState) {
+        mineTriggered = true;
+        Serial.println("Mine triggered");
+    }
+    previousState = currentState;
 }
